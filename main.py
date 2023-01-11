25000cf5810a031cdd0762fa83a45325c0fb9e81
gery28 <119705143+gery28@users.noreply.github.com>
2023-01-11 20:16:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,105 @@
+import time
 import pygame
-
-WIDTH, HEIGHT = 800, 400
-BG_COLOR = (140, 137, 246)
-BIRD_SPEED = 1
-SIZE=100
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+screen_width=800
+screen_height=600
+screen=pygame.display.set_mode((screen_width,screen_height))
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+BLUE=(50,153,213)
+RED=(213,50,0)
+GREEN=(0,255,0)
+YELLOW=(255,255,102)
 
-bird_surf_1 = pygame.image.load('robot/robot/Run_1.png').convert_alpha()
-bird_surf_2 = pygame.image.load('robot/robot/Run_2.png').convert_alpha()
-bird_surf_3 = pygame.image.load('robot/robot/Run_3.png').convert_alpha()
-bird_surf_4 = pygame.image.load('robot/robot/Run_4.png').convert_alpha()
-bird_surf = [pygame.transform.scale(bird_surf_1,(SIZE,SIZE)), pygame.transform.scale(bird_surf_2,(SIZE,SIZE)), pygame.transform.scale(bird_surf_3,(SIZE,SIZE)), pygame.transform.scale(bird_surf_4,(SIZE,SIZE))]
-bird_surf_reverse = [pygame. transform. flip(pygame.transform.scale(bird_surf_1,(SIZE,SIZE)),True,False),pygame. transform. flip(pygame.transform.scale(bird_surf_2,(SIZE,SIZE)),True,False),pygame. transform. flip(pygame.transform.scale(bird_surf_3,(SIZE,SIZE)),True,False),pygame. transform. flip(pygame.transform.scale(bird_surf_4,(SIZE,SIZE)),True,False)]
+snake_block=10
+snake_speed=30
 
-'''
-Képek tárolása egyszerűbben:
-bird_surf = []
-for index in range(1, 5):
-    bird_surf.append((pygame.image.load(f'img/bird{index}.png')).convert_alpha())
-'''
 
-bird_index = 0
-bird_rect = bird_surf[bird_index].get_rect(midleft=(270, HEIGHT / 2))
-game_font = pygame.font.SysFont('arial', 60)
-counter = 0
-running = True
-bird_foward=True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        # if event.type== pygame.KEYDOWN:
-        #     if event.key == pygame.K_LEFT:
-        #         if BIRD_SPEED>0:
-        #             BIRD_SPEED-=1
-        #     if event.key == pygame.K_RIGHT:
-        #         BIRD_SPEED+=1
-    screen.fill(BG_COLOR)
-    counter += 1
-    if counter % 7 == 0:
-        bird_index += 1
-    if bird_index > len(bird_surf) - 1:
-        bird_index = 0
-    if bird_rect.right < 534 and bird_foward:
-        bird_rect.left += BIRD_SPEED
-    elif not bird_rect.right < 534 and bird_foward:
-        bird_foward = False
-        BIRD_SPEED+=1
-        bird_rect = bird_surf[0].get_rect(midright=(533, HEIGHT / 2))
-    elif bird_rect.left > 266 and not bird_foward:
-        bird_rect.right -= BIRD_SPEED
-    elif not bird_rect.left > 266 and not bird_foward:
-        bird_foward = True
-        BIRD_SPEED+=1
-        bird_rect = bird_surf[0].get_rect(midleft=(267, HEIGHT / 2))
-    if bird_foward:
-        screen.blit(bird_surf[bird_index], bird_rect)
-    else:
-        screen.blit(bird_surf_reverse[bird_index], bird_rect)
-    score_surf = game_font.render('Speed: ' + str(BIRD_SPEED), True, (255, 0, 0))
-    screen.blit(score_surf,(0,0))
+font_style=pygame.font.SysFont('Arial',50)
+
+def message(msg,color):
+    text=font_style.render(msg,True,color)
+    screen.blit(text,(screen_width/2,screen_height/2))
+def score_message(msg,color):
+    text = font_style.render(msg, True, color)
+    screen.blit(text, (40, 40))
+clock=pygame.time.Clock()
+pygame.display.set_caption("Snake")
+
+def print_snake(snake_block,snake_list):
+    for cords in snake_list:
+        pygame.draw.rect(screen,BLACK,(cords[0],cords[1],snake_block,snake_block))
+
+def gameloop():
+    game_over = False
+    snake_x = screen_width / 2
+    snake_y = screen_height / 2
+
+    snake_y_change = 0
+    snake_x_change = 0
+
+    snake_list=[]
+    length_of_snake=1
+
+    can_move_x=True
+    can_move_y=True
+    food_x=round(random.randrange(0,screen_width-snake_block)/10.0)*10.0
+    food_y=round(random.randrange(0,screen_height-snake_block)/10.0)*10.0
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and can_move_y:
+                    snake_y_change = -snake_block
+                    snake_x_change = 0
+                    can_move_y=False
+                    can_move_x=True
+                if event.key == pygame.K_DOWN and can_move_y:
+                    snake_y_change = snake_block
+                    snake_x_change = 0
+                    can_move_y=False
+                    can_move_x=True
+                if event.key == pygame.K_LEFT and can_move_x:
+                    snake_x_change = -snake_block
+                    snake_y_change = 0
+                    can_move_x=False
+                    can_move_y=True
+                if event.key == pygame.K_RIGHT and can_move_x:
+                    snake_x_change = snake_block
+                    snake_y_change = 0
+                    can_move_x=False
+                    can_move_y=True
+
+        if snake_x >= screen_width or snake_x < 0 or snake_y >= screen_height or snake_y < 0:
+            game_over = True
+        snake_x += snake_x_change
+        snake_y += snake_y_change
+        screen.fill(WHITE)
+        pygame.draw.rect(screen, BLUE, (food_x, food_y, snake_block, snake_block))
+        snake_head=[snake_x,snake_y]
+        snake_list.append(snake_head)
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
+        print_snake(snake_block,snake_list)
+
+        # for coord in snake_list[:-1]:
+        #     if (coord)==snake_head:
+        #         game_over=True
+        score_message(("Your score: "+str(length_of_snake)), BLACK)
+        pygame.display.update()
+        if snake_x==food_x and snake_y==food_y:
+            food_x = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
+            food_y = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
+            length_of_snake+=1
+        clock.tick(snake_speed)
+
+    message("You lost", RED)
     pygame.display.update()
-    clock.tick(60)
+    time.sleep(2)
 
-pygame.quit()
+    pygame.quit()
+    quit()
+gameloop()
